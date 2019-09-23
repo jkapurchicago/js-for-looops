dc1cf4bd54e314b803775f2c2c094c879ec0cdc2
Jatin Kapur <jkapur.work@gmail.com>
2019-09-22 20:45:06 -0500

    first commit

---
+++ for-vs-while-loop.js
@@ -0,0 +1,42 @@
+//For loops
+
+console.log("------------------------------------------------------------"); 
+
+var str = "hello";
+
+for(var i = 0; i < str.length; i++){
+  console.log(str[i]);
+}
+
+
+console.log("------------------------------------------------------------"); 
+
+var luiza = "coffee"; 
+
+for(var a = 0; a < luiza.length; a++) { 
+    console.log(luiza[a]); 
+}
+
+console.log("------------------------------------------------------------"); 
+
+//Implementing using a while loop
+
+var str1 = "hello";
+var count = 0;   
+ 
+while(count < str1.length) {
+ console.log(str1[count]);
+ count++;
+}
+
+
+console.log("------------------------------------------------------------");  
+
+var luiza2 = "coffee";
+var count = 0; 
+
+while(count < luiza2.length) { 
+    console.log(luiza2[count])
+    count++; 
+} 
+
---
+++ strictly-for-loops.js
@@ -0,0 +1,4 @@
+for(var i =0; i <16; i+=8){ 
+    console.log(i); 
+}
+
